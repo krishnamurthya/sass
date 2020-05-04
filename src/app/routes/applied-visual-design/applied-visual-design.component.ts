3772e8da105b80072ce395bc4c40625621d72390
krishnamurthybtech <krishnamurthybtech@gmail.com>
2020-05-04 15:35:23 +0530

    Applied Visual Design: Create Visual Balance Using the text-align Property

--- src/app/routes/applied-visual-design/applied-visual-design.component.ts
+++ src/app/routes/applied-visual-design/applied-visual-design.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-applied-visual-design',
   templateUrl: './applied-visual-design.component.html',
-  styleUrls: ['./applied-visual-design.component.sass']
+  styleUrls: ['./applied-visual-design.component.scss']
 })
 export class AppliedVisualDesignComponent implements OnInit {
 
