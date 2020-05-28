11a247f5449863a344e1f46b1169d5c63e46a707
krishnamurthybtech <krishnamurthybtech@gmail.com>
2020-05-28 16:06:56 +0530

    Applied Accessibility: Avoid Colorblindness Issues by Carefully Choosing Colors that Convey Information

--- src/app/routes/applied-accessibility/applied-accessibility.component.ts
+++ src/app/routes/applied-accessibility/applied-accessibility.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-applied-accessibility',
   templateUrl: './applied-accessibility.component.html',
-  styleUrls: ['./applied-accessibility.component.sass']
+  styleUrls: ['./applied-accessibility.component.scss']
 })
 export class AppliedAccessibilityComponent implements OnInit {
 
